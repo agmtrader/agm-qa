ab9c176f2c04b60c369e11f410683c4276980d32
Andres Aguilar <aguilarcarboni@gmail.com>
2025-08-06 09:50:35 -0600

    Update IBKRApplicationForm to use getApplicationDefaults for default values

--- src/components/apply/form/IBKRApplicationForm.tsx
+++ src/components/apply/form/IBKRApplicationForm.tsx
@@ -88,7 +88,7 @@ const IBKRApplicationForm = () => {
 
   const form = useForm<Application>({
     resolver: zodResolver(application_schema),
-    defaultValues: individual_form,
+    defaultValues: getApplicationDefaults(application_schema),
     mode: 'onChange',
     shouldUnregister: false,
   });
